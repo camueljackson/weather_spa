223f109fc53a4f68683482bd15e842af9730abfa
vagrant <camille.a.pelletier@gmail.com>
2018-08-16 18:03:09 +0000

    updated app.jsx and index.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            key: 'value'
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+
+            <div> </div>
+
+            {/* <MessageList messageList={this.state.messages}/>
+            <ChatBar addMessage={this.addMessage} currentUser={this.state.currentUser.name}/> */}
+
+            </div>
+        );
+    }
+  }
+
+  export default App;
